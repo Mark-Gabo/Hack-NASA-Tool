5a2c9089df8b9f66176bad754594ebdbe2129150
Mark Anthony Gabo <65881629+Mark-Gabo@users.noreply.github.com>
2020-08-11 09:15:42 +0800

    Add files via upload

---
+++ NASA-hacking-tool.js
@@ -0,0 +1,25 @@
+setTimeout(function() {
+  console.log("NASA Hack .10%");
+  setTimeout(function() {
+    console.log("NASA Hack..20%");
+  setTimeout(function() {
+  console.log("NASA Hack  ....40%");
+  setTimeout(function() {
+    console.log("NASA Hack......60%");
+   setTimeout(function() {
+  console.log("NASA Hack  ........80%");
+   setTimeout(function() {
+    console.log("NASA Hack ..........100%");
+    setTimeout(function() {
+  console.log("Hacking NASA is succesfull");
+  setTimeout(function() {
+    console.log ("warring you are at risk ");
+  }, 8000);
+}, 7000);
+  }, 6000);
+}, 5000);
+  }, 4000);
+}, 3000);
+  }, 2000);
+}, 1000);
+console.log("satrt to hack NASA");
